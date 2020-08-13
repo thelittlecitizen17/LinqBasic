0454cb464257ff84ca26a6707986c0158587020f
Yakov Pizenberg <thelittlecitizen17@gmail.com>
2020-08-13 23:13:45 +0300

    adding the final answer

---
+++ LinqBasic/PersonRetriver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using LinqBasic.Abstract;
+
+
+namespace LinqBasic
+{
+    class PersonRetriver: IPersonRetriver 
+    {
+
+        public IEnumerable<int> GetMalesId(IEnumerable<Person> persons)
+        {
+            var result = persons.Where(person => person.PersonGender == Gender.Male).Select(id=>id.Id);
+
+
+            return result;
+        }
+
+        public IEnumerable<Person> GetOddIds(IEnumerable<Person> persons)
+        {
+            var result = persons.Where(person => person.Id % 2 != 0);
+            return result;
+        }
+
+        public IEnumerable<Person> OrderById(IEnumerable<Person> persons)
+        {
+            var result = persons.OrderBy(person => (person.Id)*(-1));
+            return result;
+        }
+
+    }
+}
---
+++ LinqBasic/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using LinqBasic.Abstract;
+
+namespace LinqBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Person> persons = new List<Person>()
+                                       {
+                                           new Person()
+                                               {
+                                                   Id = 1,
+                                                   Name = "Tomer",
+                                                   PersonGender = Gender.Male
+                                               },
+                                           new Person()
+                                               {
+                                                   Id = 2,
+                                                   Name = "Tami",
+                                                   PersonGender = Gender.Female
+                                               },
+                                           new Person()
+                                               {
+                                                   Id = 3,
+                                                   Name = "Nadav",
+                                                   PersonGender = Gender.Male
+                                               }
+                                           };
+
+
+            PersonRetriver personRetriver = new PersonRetriver();
+            personRetriver.GetMalesId(persons);
+            personRetriver.OrderById(persons);
+            personRetriver.GetOddIds(persons);
+        }
+    }
+}
